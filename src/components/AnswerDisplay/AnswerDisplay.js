8f27ac9e89b17a6526cc03eeac34d3daab711ce0
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2018-08-24 11:20:14 +0800

    Fix invalid property

--- src/components/AnswerDisplay/AnswerDisplay.js
+++ src/components/AnswerDisplay/AnswerDisplay.js
@@ -4,8 +4,8 @@ import './AnswerDisplay.css';
 const AnswerDisplay  = (props) => {
     return (
         <div className='interest-calculator__answer-display'>
-          <h4 class="title is-4">Answer:</h4>
-          <h5 class="subtitle is-5">{props.answer}</h5>
+          <h4 className="title is-4">Answer:</h4>
+          <h5 className="subtitle is-5">{props.answer}</h5>
         </div>
     );
 }
